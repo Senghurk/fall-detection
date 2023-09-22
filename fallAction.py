0f9766e066f9cc82eabe284abbbde3781c8bd2fa
Hseng Hurk <bin4ryhunt3r@gmail.com>
2023-09-22 19:06:18 +0700

    update commit

--- fallAction.py
+++ fallAction.py
@@ -3,13 +3,17 @@ import numpy as np
 import collections
 
 class fallAction:
+
     def __init__(self):
         self.init = False
         self.detectedAngles = collections.deque([0]*5, 5)
 
     def check(self, angle):
+
         if angle is not None:
+
             if not self.init:
+                
                 self.detectedAngles.appendleft(angle)
                 if all(i >= 70 for i in self.detectedAngles) and all(i <= 100 for i in self.detectedAngles):
                     print("Fall Detected!!!")
